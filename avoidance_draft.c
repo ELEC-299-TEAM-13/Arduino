cd7d4f5ac58a1506c6e349ce36a662d2730067b4
chen <64240171+Tateha-A@users.noreply.github.com>
2021-06-15 10:41:35 -0400

    Update avoidance_draft.c

--- avoidance_draft.c
+++ avoidance_draft.c
@@ -4,11 +4,12 @@ float width2;
 float depth2;
 float obst1;		//distance from start to obatacle 1
 float obst2;		//distance between obstacle 1/2
-float destination;	//distance from obstacle 1/2 to destination
+float destination;	//distance from obstacle 1/2 to destination or full distance
 int obst1Flag; 		//1 for engaged obst 1
 int obst2Flag; 		//1 for engaged obst 2
+int rightFlag;    //1 for special right turn
 
-
+/*
 void avoidObstacles()
 {
   delay(200);
@@ -18,7 +19,7 @@ void avoidObstacles()
   delay(500);
   if (obst2Flag == 0) // -x travel
   {
-    width1 = overcomeCheck() + 25;
+    width1 = overcomeCheck() + 25; // forward 25 for fully passing (vehicle body length)
   }
   else // now on obst 2
   {
@@ -31,8 +32,8 @@ void avoidObstacles()
   delay(500);
   if (obst2Flag == 0) // +y travel
   {
-    simpleStraight(15);
-    depth1 = overcomeCheck() + 40;
+    simpleStraight(15); 			// forward 15 for fully engaging
+    depth1 = overcomeCheck() + 40; 	// 15 + 25 = 40
   }
   else // obst 2
   {
@@ -58,8 +59,138 @@ void avoidObstacles()
   delay(700);
   //  while (1){};
 }
-
+*/
 // driveStraight & simpleStraight need return distance
+
+void avoidObstacles()
+{
+  wait(200);
+  backUp(3);
+  wait(500);
+  turnLeft(13); // facing left
+  wait(500);
+  if (ultraSonic() < 20)// obstacle on the left side blocking the left turn
+  {
+    turnRight(33);  // facing to right
+      if (obst2Flag == 0) // +x travel
+      {
+        width1 = overcomeCheck() + 25;  // forward 25 for fully passing (vehicle body length)
+      }
+      else // now on obst 2
+      {
+        width2 = overcomeCheck() + 25;
+      }
+      simpleStraight(25);
+
+      rightFlag = 1; // marking a right turn
+
+      wait(500);
+      turnLeft(13); // facing forward
+      wait(500);
+      if (obst2Flag == 0) // +y travel
+      {
+        simpleStraight(15);
+        depth1 = overcomeCheck() + 40;
+      }
+      else // obst 2
+      {
+        simpleStraight(15);
+        depth2 = overcomeCheck() + 40;
+      }
+      simpleStraight(25);
+      
+      wait(500);
+      // overcome the obstacle
+      turnLeft(13); // +x travel
+      wait(500);
+      if (obst2Flag == 0)
+      {
+        simpleStraight(width1);
+      }
+      else // obst 2
+      {
+        simpleStraight(width2);
+      }
+      wait(500);
+      turnRight(13); // back to m line
+      wait(700);
+  }
+  else // default left turn
+  {
+    //
+      if (obst2Flag == 0) // -x travel
+      {
+        width1 = overcomeCheck() + 25;  // forward 25 for fully passing (vehicle body length)
+      }
+      else // now on obst 2
+      {
+        width2 = overcomeCheck() + 25;
+      }
+      simpleStraight(25);
+      
+      wait(500);
+      turnRight(13); // facing forward
+      wait(500);
+      
+      if (obst2Flag == 0) // +y travel
+      {
+        simpleStraight(15); // forward 15 for fully engaging
+        depth1 += overcomeCheck() + 40; // 15 + 25 = 40
+      }
+      else // obst 2
+      {
+        simpleStraight(15);
+        depth2 += overcomeCheck() + 40;
+      }
+      simpleStraight(25);
+      
+      wait(500);
+      // overcome the obstacle
+      turnRight(13); // facing right +x travel
+
+      while (1) // front left back right
+      {
+        wait(300);
+        if (ultraSonic() < 20)
+        {
+          turnLeft(13); // facing forward
+          wait(300);
+          simpleStraight(15);
+          if (obst2Flag == 0) // +y travel
+          {
+            depth1 += 15;
+          }
+          else // obst 2
+          {
+            depth2 += 15;
+          }
+          wait(300);
+          turnRight(13);  // facing right
+          continue; //  check again
+        }
+        else
+        {
+          break;  // nothing blocking 
+        }
+      }
+      
+      wait(500);
+      if (obst2Flag == 0)
+      {
+        simpleStraight(width1);
+      }
+      else // obst 2
+      {
+        simpleStraight(width2);
+      }
+      wait(500);
+      turnLeft(13); // back to m line
+      wait(700);
+      //  while (1){};
+  }
+}
+
+
 
 float overcomeCheck ()
 {
